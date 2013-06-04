c4e5071bc2f2f64d8adac78c73faf73fe81a24ad
cesperanc@ <cesperanc@gmail.com>
2013-06-04 23:50:26 +0100

    ->Database name update

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -19,7 +19,7 @@ return array(
                 'params' => array(
                     'host'     => 'localhost',
                     'port'     => '3306',
-                    'dbname'   => 'wtf_db',
+                    'dbname'   => 'CETSSDB',
                 )
             )
         )
